090fdd616b7aa864fd4eba6f0947bc2c91d8abab
Lorenzo-UNI <s1118107@studenti.univpm.it>
2025-07-02 17:37:27 +0200

    implemented "Attrezzatura Sportiva" page

---
+++ CityBeach/Model/EquipmentType.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class EquipmentType(Enum):
+    PADEL_RACKETS = "Racchetta da Padel"
+    PADEL_BALLS = "Pallina da Padel"
+    BEACH_TENNIS_RACKETS = "Racchetta da Beach Tennis"
+    BEACH_TENNIS_BALLS = "Pallina da Beach Tennis"
+    BEACH_VOLLEYBALLS = "Palla da Beach Volley"
